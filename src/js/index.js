4b236a13a0ffbf32637461135c9b07584291f9c3
Daniel Abad Noriega <daniel.abad.noriega@gmail.com>
2022-11-24 17:56:33 +0100

    OB_M3_T12_E1

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,25 @@
-// n = n-1 + n -2
-// n=1  n= 0 - 2
-// n= 2 n= 1:  + 2 -1
-// n =3 n = 3-1 + 3-2 -> 2 + 1
-// n = 4 -- n = 4-1 + 3 - 2 -> 3 + 2
-//Resultado [1, 1, 2, 3, 5, 8]
-
 function fibonacci(num) {
+  if (num <= 1) {
+    return [1];
+  }
+  if (num <= 2) {
+    return [1, 1];
+  }
   if (num >= 2) {
     let fib = [1, 1];
-    let i = 2;
-    for (i; i <= num; i++) {
-      res = i - 1 + (i - 2);
-      fib.push();
+    let i = 1;
+    for (i; i <= num - 2; i++) {
+      res = fib[i] + fib[i - 1];
+      fib.push(res);
     }
+    return fib;
   }
 
   throw new Error("\nIntroduce un número mayor 2");
 }
 
 try {
+  console.log(fibonacci(6));
 } catch (error) {
   console.log(`[Error] Error en la ejecución del código: ${error.message}`);
 }
